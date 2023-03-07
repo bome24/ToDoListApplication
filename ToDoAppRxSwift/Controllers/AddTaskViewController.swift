20d361690aab2fd5dc52d0005eeb31ff20cd4d86
bome24 <81296575+bome24@users.noreply.github.com>
2023-03-08 02:39:55 +0900

    FEAT: RxSwift pod installed

--- ToDoAppRxSwift/Controllers/AddTaskViewController.swift
+++ ToDoAppRxSwift/Controllers/AddTaskViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import RxSwift
 
 class AddTaskViewController: UIViewController {
+    
+    @IBOutlet weak var prioritySegControl: UISegmentedControl!
+    @IBOutlet weak var textField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+    @IBAction func saveTask() {
+        
+        guard let priority = Priority(rawValue:self.prioritySegControl.selectedSegmentIndex),
+              let title = self.textField.text else {
+            return
+        }
+        
+        let task = Task(title: title, priority: priority)
+        
+    }
+    
 }
